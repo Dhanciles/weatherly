30b2f3361c3501e744356a217a18acb5fee09a60
Derek Hanciles <drhanciles@icloud.com>
2018-09-09 18:54:10 -0600

    Skelteon for api fetch method has been implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { data } from './mockdata.js'
+// import {apiKey} from './API.js'
 import Welcome from './Welcome'
 import CurrentWeather from './CurrentWeather'
 import SevenHour from './SevenHour'
@@ -13,14 +14,37 @@ export default class App extends Component {
 
       this.state = {
         userName: '', 
-        location: ''
+        location: '', 
+        currentWeatherData: {}, 
+        sevenHourData: [], 
+        tenDayData: [] 
       }
       this.takeNameAndLocation = this.takeNameAndLocation.bind(this); 
   }
 
   takeNameAndLocation(name, location) {
-
     this.setState({userName: name, location: location})
+  }
+
+  cleanLocation() {
+    // let stateAbbrv = this.state.location.slice(0, -2); 
+    // console.log(stateAbbrv); 
+  }
+
+  getZipCode(location) {
+    
+  }
+
+  componentDidUpdate() {
+    this.cleanLocation()
+
+    // // let city = this.state.location.
+
+    // // let apiCurrentWeather = `http://api.wunderground.com/api/${apiKey}/forecast/q/${this.state.location}/.json`
+    // // let apiSevenHour = 
+    // // let apiTenDay = 
+
+    // // .fetch()
   }
   
   render() {
