a2f832c8b6dfd9bdef534583dc96b1bf5f0b7784
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-13 09:04:22 -0600

    Fix long lines in Current weather

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -11,14 +11,17 @@ export default class CurrentWeather extends Component {
 
   render() {
     if (this.props.weatherData.forecast && this.props.tempData.current_observation) {
+
+      let weatherPrefix = this.props.weatherData.forecast.simpleforecast.forecastday[0]
+
       return(
         <div className="currWeather">
           <h1>{this.props.cityData}, {this.props.stateData}</h1>
-          <h2>{this.props.weatherData.forecast.simpleforecast.forecastday[0].date.weekday}, {this.props.weatherData.forecast.simpleforecast.forecastday[0].date.monthname_short}. {this.props.weatherData.forecast.simpleforecast.forecastday[0].date.day}</h2>
+          <h2>{weatherPrefix.date.weekday}, {weatherPrefix.date.monthname_short}. {weatherPrefix.date.day}</h2>
           <h1>{this.props.tempData.current_observation.temperature_string}</h1>
-          <img className="weatherPic" src={this.props.weatherData.forecast.simpleforecast.forecastday[0].icon_url}/>
-          <h3>High: {this.props.weatherData.forecast.simpleforecast.forecastday[0].high.fahrenheit}º <span> - Low: {this.props.weatherData.forecast.simpleforecast.forecastday[0].low.fahrenheit}º</span></h3>
-          <h3>{this.props.weatherData.forecast.simpleforecast.forecastday[0].conditions}</h3> 
+          <img className="weatherPic" src={weatherPrefix.icon_url}/>
+          <h3>High: {weatherPrefix.high.fahrenheit}º <span> - Low: {weatherPrefix.low.fahrenheit}º</span></h3>
+          <h3>{weatherPrefix.conditions}</h3> 
           <h3>{this.props.weatherData.forecast.txt_forecast.forecastday[0].fcttext}</h3>
         </div>
       )
